11e64141fbe91e67ce37c7ed2b40a073dacd6d36
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-05-20 11:38:44 +0500

    Main compenent ulandi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,57 @@ import React from "react";
 import MainComponent from "./MainComponent";
 
 function App() {
+  const [gameState, updateGameState] = useState(clearState);
+  const [isXChance, updateIsXChance] = useState(false);
+
+  const onUserClicked = (index) => {
+    let strings = Array.from(gameState);
+    if (strings[index]) return;
+    strings[index] = isXChance ? "X" : "0";
+    updateIsXChance(!isXChance);
+    updateGameState(strings);
+  };
+
+  const clearGame = () => {
+    updateGameState(clearState);
+  };
+  useEffect(() => {
+    let winner = checkWinner();
+    if (winner) {
+      clearGame();
+      alert(`Ta da ! ${winner} won the Game !`);
+    }
+  }, [gameState]);
+
+  const checkWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    console.log(
+      "Class: App, Function: checkWinner ==",
+      gameState[0],
+      gameState[1],
+      gameState[2]
+    );
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        gameState[a] &&
+        gameState[a] === gameState[b] &&
+        gameState[a] === gameState[c]
+      ) {
+        return gameState[a];
+      }
+    }
+    return null;
+  };
   return (
     <div className="app-header">
       <p className="heading-text">React Tic Tac Toe - 2020</p>
