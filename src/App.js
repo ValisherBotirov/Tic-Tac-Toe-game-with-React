a1266bed7ccfc07d62e0a9dbcc355a07d3870e3d
Valisher Botirov <valisherbotirov1218@gmail.com>
2022-05-20 11:37:29 +0500

    Main compenent integratsiya qildim App jsga

--- src/App.js
+++ src/App.js
@@ -1,7 +1,63 @@
 import React from "react";
+import MainComponent from "./MainComponent";
 
 function App() {
-  return <div>This is App commonent</div>;
+  return (
+    <div className="app-header">
+      <p className="heading-text">React Tic Tac Toe - 2020</p>
+      <div className="row jc-center">
+        <MainComponent
+          className="b-bottom-right"
+          onClick={() => onUserClicked(0)}
+          state={gameState[0]}
+        />
+        <MainComponent
+          className="b-bottom-right"
+          onClick={() => onUserClicked(1)}
+          state={gameState[1]}
+        />
+        <MainComponent
+          className="b-bottom"
+          onClick={() => onUserClicked(2)}
+          state={gameState[2]}
+        />
+      </div>
+      <div className="row jc-center">
+        <MainComponent
+          className="b-bottom-right"
+          onClick={() => onUserClicked(3)}
+          state={gameState[3]}
+        />
+        <MainComponent
+          className="b-bottom-right"
+          onClick={() => onUserClicked(4)}
+          state={gameState[4]}
+        />
+        <MainComponent
+          className="b-bottom"
+          onClick={() => onUserClicked(5)}
+          state={gameState[5]}
+        />
+      </div>
+      <div className="row jc-center">
+        <MainComponent
+          className="b-right"
+          onClick={() => onUserClicked(6)}
+          state={gameState[6]}
+        />
+        <MainComponent
+          className="b-right"
+          onClick={() => onUserClicked(7)}
+          state={gameState[7]}
+        />
+        <MainComponent onClick={() => onUserClicked(8)} state={gameState[8]} />
+      </div>
+      <button className="clear-button" onClick={clearGame}>
+        Clear Game
+      </button>
+      <p className="fc-aqua fw-600">The Indian Dev</p>
+    </div>
+  );
 }
 
 export default App;
